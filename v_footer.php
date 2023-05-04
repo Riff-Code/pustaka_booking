e4e50985e5d4ced82c580ad6e3b307480347d9b0
Muhammad Arif Ramadhan <102840761+Riff-CodeZero@users.noreply.github.com>
2023-05-04 22:10:57 +0700

    Add files via upload

---
+++ v_footer.php
@@ -0,0 +1,7 @@
+        <div>
+            <footer>
+                <a href="http://www.RentalBuku.com">RentalBuku 2023.Muh Arif Ramadhan</a>
+            </footer>
+        </div>
+    </body>
+</html
